03939775cd4b987023fe674b62533f182b12bc63
Suni <suni.suni571@gmail.com>
2018-01-22 11:07:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ function logWhisper(string){
 logWhisper('HELLO');
 
 function sayHiToGrandma(string){
-	if(string.toLowerCase === string){
+	if(string === string.toLowerCase){
 		return "I can\'t hear you!";
 		}
-	else if(string.toUpperCase === string){
+	else if(string === string.toUpperCase){
 		return "YES INDEED!";
 		}
 	else if(string === "I love you, Grandma."){
