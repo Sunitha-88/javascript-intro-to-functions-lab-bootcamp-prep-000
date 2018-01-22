cd8473556ef534807974428cfba63e25c8dbab76
Suni <suni.suni571@gmail.com>
2018-01-22 10:28:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,22 @@ function whisper(string){
   return string.toLowerCase()
 }
 whisper('HELLO')
+function logShout(string){
+		
+		var upperCase = string.toUpperCase();
+		if(upperCase === string){
+			const spy=spyOn(console,'log'){
+				}
+			console.log(spy);
+			}
+
+		var lowerCase = string.toLowerCase();
+		
+			console.log(spyOn())
+			
+
+}
+
+
+logShout('HELLO').spy;
+logShout('hello').spyOn(console,'log');
